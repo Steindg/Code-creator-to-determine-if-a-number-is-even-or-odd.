635ea17b8e00ccbf81551b868eb48fa690479af7
Steindg <112285143+Steindg@users.noreply.github.com>
2026-04-10 17:37:44 -0300

    Update Code creator to determine if a number is even or odd..py

--- Code creator to determine if a number is even or odd..py
+++ Code creator to determine if a number is even or odd..py
@@ -1,15 +1,15 @@
-arquivo=open("par or impar.py","w")
+arquivo=open("even  or odd.py","w")
 par=False
 num=0
 arquivo.write("number=int(input('Enter a number: '))\n")
 while num<=10000000:
     if par==False:
         arquivo.write(f"if {num}==number:\n")
-        arquivo.write("    print('Par is True')\n")
+        arquivo.write("    print('even is True')\n")
         par=True
     else:
         arquivo.write(f"if {num}==number:\n")
-        arquivo.write("    print('Par is False')\n")
+        arquivo.write("    print('even is False')\n")
         par=False
     num+=1
 arquivo.close()
